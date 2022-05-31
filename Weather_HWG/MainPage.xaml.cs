29a652218013703d08d0903a1bfefccc2bbe99f1
Sma5aka <54479462+Sma5aka@users.noreply.github.com>
2022-05-31 16:37:21 +1000

    first ci

---
+++ Weather_HWG/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace Weather_HWG
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+        async void new_ac()
+        {
+            var current = Connectivity.NetworkAccess;
+
+            if (current == NetworkAccess.Internet)
+            {
+                await DisplayAlert("Success", "INTERNET IS ON", "Ok");
+            }
+            else
+            {
+                await DisplayAlert("Error", "INTANATA NET", "neOk");
+            }
+        }
+    }
+}
